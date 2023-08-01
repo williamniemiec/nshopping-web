0d6d6b9deaf855c792ba5ac6669273bcfcca7665
William Niemiec <williamniemiec@hotmail.com>
2023-08-01 10:17:43 -0300

    final slash removed

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -3,6 +3,6 @@
  */
 export const API_CONFIG = {
 
-  baseUrl: 'https://wniemiec-api-nshopping.onrender.com/',
+  baseUrl: 'https://wniemiec-api-nshopping.onrender.com',
   bucketBaseUrl: 'https://nshop.s3.sa-east-1.amazonaws.com'
 }
